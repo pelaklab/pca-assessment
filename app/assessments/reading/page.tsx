6e8d17b2f1e3362a17293213df248a8504543b7f
katieli-waymark <katie.li@waymarkcare.com>
2024-11-09 15:09:25 -0700

    resize the paragraphs to read

--- app/assessments/reading/page.tsx
+++ app/assessments/reading/page.tsx
@@ -5,7 +5,7 @@ import { ParagraphImages } from "./reading";
 export default function ReadingPage() {
 
     return (
-        <main className="flex flex-col items-center justify-between p-24">
+        <main className="flex flex-col items-center justify-between pt-24">
             <ParagraphImages />
         </main>
     );
