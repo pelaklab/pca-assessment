032a14e2057a7774d4804c78d3b5d1d4a7415fef
katieli-waymark <katie.li@waymarkcare.com>
2024-12-05 18:50:55 -0700

    Contact page revisions

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,31 +8,59 @@ export default function Contact() {
           <Image src="/icons/team.svg" alt="" width={93} height={93} className="mb-10" />
           <h1 className="md:text-4xl text-2xl font-medium text-left">About Us</h1>
         </div>
-        <p>Our website functions as an <span className="font-semibold">
-          initial clinical screening tool</span> to assess the
-          visual processing and spatial recognition skills of patients, to
-          improve the diagnosis of patients with posterior cortical atrophy (PCA).
-          PCA is a rare form of Alzheimer&apos;s disease often presents with visual issues
-          and is misdiagnosed because of its atypical presentation</p>
-
-        <p>This work is part of&nbsp;
-          <a href="https://www.uchealth.org/provider/victoria-pelak-md/"
-            className="font-medium text-forget-me-not-purple underline hover:no-underline">
-            Dr. Victoria Pelak&apos;s
-          </a>
-          &nbsp;clinical research.
-
-          She is a neuro-opthamologist at the University of Colorado Anschutz Medical Campus,
-          a professor in the Department of Neurology and Ophthalmology.
-        </p>
         <p>
-          If you have any questions
-          about tasks, scoring or general questions of the screening assessment on this website,
-          you can reach Colorado Posterior Cortical Atrophy Research at&nbsp;
-          <a href="mailto:COPCA@CUAnschutz.edu "
+          This resource was developed by <a href="https://medschool.cuanschutz.edu/neurology/faculty"
+            className="text-forget-me-not-purple underline hover:no-underline">
+            Dr. Victoria Pelak</a>&nbsp;
+          and her research team to provide a
+          user-friendly set of tools for researchers and clinicians to screen for visual cortical
+          dysfunction. The tool is recommended to help identify those with posterior cortical
+          atrophy (PCA) syndrome, which is most commonly caused by Alzheimer&apos;s disease and,
+          less commonly, dementia with Lewy bodies. Dr. Pelak leads Colorado Posterior Cortical
+          Atrophy Support, and the webpages and additional resources can be found&nbsp;
+          <a href="https://medschool.cuanschutz.edu/pca"
             className="text-forget-me-not-purple underline hover:no-underline"
-          >email</a>
+          >
+            here
+          </a>
+          .
         </p>
+        <div>
+          <p>
+            VisCorD Website Team:
+          </p>
+          <ul className="list-disc pl-5">
+            <li>
+              Dr. Victoria Pelak
+            </li>
+            <li>
+              Lily Noteboom
+            </li>
+            <li>
+              Asher Mahood
+            </li>
+            <li>
+              Katie Li - Website creator
+            </li>
+            <li>
+              Jessica St. Onge - Design and UX research -&nbsp;
+              <a href="https://www.jkstonge.com/"
+                className="text-forget-me-not-purple underline hover:no-underline">website</a>
+            </li>
+          </ul>
+        </div>
+        <div className="space-y-1">
+          <p>
+          To contact about tasks, scoring or general questions of the screening assessment on this website:&nbsp;
+            <a href="mailto:copca@cuanschutz.edu"
+              className="text-forget-me-not-purple underline hover:no-underline">email</a>
+          </p>
+          <p>
+            To contact about the website:&nbsp;
+            <a href="mailto:katie.li@cuanschutz.edu"
+              className="text-forget-me-not-purple underline hover:no-underline">email</a>
+          </p>
+        </div>
       </div>
     </main>
   );
