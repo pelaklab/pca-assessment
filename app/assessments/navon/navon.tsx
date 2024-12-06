bbadbc7aea9cb665b0b3c8f85eb6b0616c999031
katieli-waymark <katie.li@waymarkcare.com>
2024-12-05 18:14:57 -0700

    Fix contact page and arrows
    arrows always show up regardless of the page

--- app/assessments/navon/navon.tsx
+++ app/assessments/navon/navon.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/16/solid';
-import { headerVisible } from '@/app/atoms/experimentAtoms';
-import { useAtomValue } from 'jotai';
 import Image from 'next/image'
 
 
@@ -12,12 +10,7 @@ const NavonImages: React.FC = () => {
     ]
     // State to track the current image index
     const [currentIndex, setCurrentIndex] = useState(0);
-    const isHeaderVisible = useAtomValue(headerVisible)
     const [indicatorsVisible, setIndicatorVisibility] = useState(true)
-
-    useEffect(() => {
-        setIndicatorVisibility(isHeaderVisible)
-    }, [isHeaderVisible])
 
     // Handler to show the next image
     const handleNextImage = () => {
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ export default function Contact() {
     <main className="h-screen w-full bg-ivory grid grid-cols-8 text-xl">
       <div className="md:col-start-2 md:col-span-5 col-span-6 col-start-2 bg-ivory space-y-6">
         <div className="pt-10">
-          <Image src="/icons/team.svg" alt="" width={93} height={93} className="mb-10"/>
+          <Image src="/icons/team.svg" alt="" width={93} height={93} className="mb-10" />
           <h1 className="md:text-4xl text-2xl font-medium text-left">About Us</h1>
         </div>
         <p>Our website functions as an <span className="font-semibold">
@@ -25,27 +25,14 @@ export default function Contact() {
           She is a neuro-opthamologist at the University of Colorado Anschutz Medical Campus,
           a professor in the Department of Neurology and Ophthalmology.
         </p>
-      </div>
-
-      <div className="md:col-start-2 md:col-span-5 col-span-6 col-start-2 bg-ivory space-y-6">
-        <h1 className="md:text-4xl text-2xl font-medium text-left">Contact Information</h1>
-        <div>
-          <p> Colorado Posterior Cortical Atrophy Research |
-            <a href="mailto:COPCA@CUAnschutz.edu "
-              className="text-forget-me-not-purple"
-
-            > email</a>
-          </p>
-          <p>Katie Li - Website creator |
-            <a href="mailto:katie.li@cuanschutz.edu"
-              className="text-forget-me-not-purple"
-            > email </a>
-          </p>
-          <p>Jessica St. Onge - Designer and UX researcher |
-            <a href="https://www.jkstonge.com/"
-              className="text-forget-me-not-purple"
-            > website </a></p>
-        </div>
+        <p>
+          If you have any questions
+          about tasks, scoring or general questions of the screening assessment on this website,
+          you can reach Colorado Posterior Cortical Atrophy Research at&nbsp;
+          <a href="mailto:COPCA@CUAnschutz.edu "
+            className="text-forget-me-not-purple underline hover:no-underline"
+          >email</a>
+        </p>
       </div>
     </main>
   );
