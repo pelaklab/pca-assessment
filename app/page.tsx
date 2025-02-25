d34056b157de4e8efca001a8d309d070d38226e7
pelaklab <pelaklab@gmail.com>
2025-02-25 16:23:49 -0700

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,8 @@ export default function Home() {
                 unexplained visual complaints following a comprehensive eye exam* see disclaimer
               </strong>
             </p>
+
+            <p><a href="https://redcap.ucdenver.edu/surveys/?s=EJLK3LN3F9WANDF4" target="_new">Click here to take the survey</a> to help the designers know more about who is utilizing this screening assessment. This information will only be accessible by the team base at University of Colorado School of Medicine.</p>
           </div>
         </div>
 
